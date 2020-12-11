4eae2add682dbc85597de1eef73243c6bc8f1a4b
bminh1762000 <vukhachungyen@gmail.com>
2020-12-11 08:57:10 +0700

    Updates

---
+++ precache-manifest.e77cc93c938c402839395f9fcbd35053.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f82322fdfe4c68d8d555d336c5e5e17",
+    "url": "/bminh1762000/.github.io/crwn-clothing/index.html"
+  },
+  {
+    "revision": "b517359dc81d6bd77af1",
+    "url": "/bminh1762000/.github.io/crwn-clothing/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4ed352c49cf29d532d5e",
+    "url": "/bminh1762000/.github.io/crwn-clothing/static/js/2.93c8fa7c.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/bminh1762000/.github.io/crwn-clothing/static/js/2.93c8fa7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "685c2c36a1b5074400e4",
+    "url": "/bminh1762000/.github.io/crwn-clothing/static/js/3.a9a34dfc.chunk.js"
+  },
+  {
+    "revision": "cb0de54c86fadf0d8d10",
+    "url": "/bminh1762000/.github.io/crwn-clothing/static/js/4.cbdeceb1.chunk.js"
+  },
+  {
+    "revision": "a56c1f890a27e8708e04",
+    "url": "/bminh1762000/.github.io/crwn-clothing/static/js/5.38ba3a0c.chunk.js"
+  },
+  {
+    "revision": "d9c8c06062f51bed246a",
+    "url": "/bminh1762000/.github.io/crwn-clothing/static/js/6.dde8bea0.chunk.js"
+  },
+  {
+    "revision": "2838aceca7c29e4535d5",
+    "url": "/bminh1762000/.github.io/crwn-clothing/static/js/7.b2d51162.chunk.js"
+  },
+  {
+    "revision": "b517359dc81d6bd77af1",
+    "url": "/bminh1762000/.github.io/crwn-clothing/static/js/main.ab3e39b6.chunk.js"
+  },
+  {
+    "revision": "8d39e000a3a3d3bd5454",
+    "url": "/bminh1762000/.github.io/crwn-clothing/static/js/runtime-main.b2c1d040.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/bminh1762000/.github.io/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/bminh1762000/.github.io/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
